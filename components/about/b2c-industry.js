677c07396c44ba1f8c0098544ca50b389b3bfd21
nick-andriam <nick@glaxier.com.au>
2021-11-09 18:07:31 +0700

    about us toggle with cards

--- components/about/b2c-industry.js
+++ components/about/b2c-industry.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import Slider from "react-slick";
+import useWindowSize, { mobileScreen, withSizeLessThan } from '../../hooks/useWindowSize';
 
 const B2cIndustry = () => {
+    let sm = withSizeLessThan(700)
+    let md = withSizeLessThan(1200)
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: sm && 1 || md && 2 || 3,
+        slidesToScroll: 1,
+        arrows: false,
+    };
     return (
-        <div>
-            b2c
+        <div className="p-20">
+            <Slider {...settings}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </Slider>
+        </div>
+    )
+}
+
+const Card = () => {
+    return (
+        <div className="w-80 h-48 bg-white shadow-sm rounded-xl p-4 m-5">
+            <div className="flex items-center justify-start py-2">
+                <div className="bg-gray-300 h-10 w-10 rounded-full" />
+                <p className="ml-4 font-bold">Insurance</p>
+            </div>
+            <p className="text-black-light py-4" style={{ fontSize: '0.68rem' }}>We’ve worked on a number of travel insurance brands. The industry is extremely competitive which makes it challenging to stay on top of competitiors in the market, but also rewarding to see that we deliver results.</p>
         </div>
     )
 }
