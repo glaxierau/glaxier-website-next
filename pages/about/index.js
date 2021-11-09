3658f28ff82db0e27c5537a73283eacdd1fb43f3
Nick Andriam <nickandriam@Nicks-MacBook-Pro.local>
2021-11-09 16:15:58 +0700

    custom head for pages

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import AboutSection from '../components/about/About'
-import ProjectSection from '../components/project/Project'
-import SectionTitle from '../components/common/SectionTitle'
-import Title from '../components/title'
-import TeamBadge from '../components/icons/TeamBadge'
+import AboutSection from '../../components/about/About'
+import ProjectSection from '../../components/project/Project'
+import SectionTitle from '../../components/common/SectionTitle'
+import Title from '../../components/title'
+import TeamBadge from '../../components/icons/TeamBadge'
+import SectionHead from '../../components/common/Head'
 const About = () => {
     return (
         <div>
+            <SectionHead title="About" description="desc desc" />
             <div className="py-2">
                 <img src="/shape.svg" alt="glaxier shape" />
                 <SectionTitle title="ABOUT" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed" />
@@ -20,7 +22,7 @@ const About = () => {
             <ProjectSection />
             <div className="lg:py-20 py-10">
                 <Title title="Glaxier Team" lineWidth={220} />
-                <div className="flex flex-wrap p-4 py-10 lg:px-20 md:px-2 px-4 justify-around items-center">
+                <div className="flex flex-wrap p-4 py-10 lg:px-20 md:px-0 px-4 justify-around items-center">
                     <TeamBadge position="Digital Advertising Specialist" />
                     <TeamBadge position="Digital Advertising Specialist" />
                     <TeamBadge position="Digital Advertising Specialist" />
