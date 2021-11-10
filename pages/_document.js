b2538fced8f4f55a4a7f296a362b81bc9f26acfb
nick-andriam <nick@glaxier.com.au>
2021-11-10 19:16:32 +0700

    blogs

--- pages/_document.js
+++ pages/_document.js
@@ -12,8 +12,6 @@ class MyDocument extends Document {
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet" />
-                    <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-                    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
                 </Head>
                 <body>
                     <Main />
