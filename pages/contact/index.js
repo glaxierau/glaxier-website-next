1424379479eac5169fe519fea767d56dc0d1797d
nick-andriam <nick@glaxier.com.au>
2021-11-12 02:46:32 +0700

    loading screen

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import SectionHead from '../../components/common/Head'
+import Loading from '../../components/loading/loading'
 
 const Contact = () => {
     return (
         <div>
             <SectionHead title="Contact Us | Glaxier" />
-            underdevelopment
+            <Loading />
         </div>
     )
 }
