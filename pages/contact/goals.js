c9db6fcfdb49a5019fdc5b28972ddec5602acb9b
nick-andriam <nick@glaxier.com.au>
2021-11-16 17:44:15 +0700

    industry in contact page

--- pages/contact/goals.js
+++ pages/contact/goals.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import GoalBox from '../../components/contact/GoalBox'
 import ContactTitle from '../../components/contact/Title'
 import AppButton from '../../components/appButton'
+import Head from '../../components/common/Head'
 import { setAllToFalse } from '../../hooks/setAllToFalse'
 import { setToTrue } from '../../hooks/setToTrue'
 import { withSizeLessThan } from '../../hooks/useWindowSize'
@@ -25,12 +26,13 @@ const Goals = () => {
     }
     return (
         <div className="h-auto">
+            <Head title="Contact | Your Goals" />
             <ContactTitle title="Share your goal with us" />
             <div className="flex justify-center flex-wrap m-auto" style={{ width: sm ? 340 : 600, flex: '1 1 160px' }}>
                 {lists.map(list => <GoalBox key={list.key} icon={list.icon} name={list.name} active={list.active} onClick={() => onChoosingaGoal(list)} />)}
             </div>
             <div className="mx-auto w-96 flex items-center justify-center py-9">
-                <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" />
+                <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/contact/industry-of-business' />
             </div>
         </div>
     )
--- pages/contact/industry-of-business.js
+++ pages/contact/industry-of-business.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import ContactTitle from '../../components/contact/Title'
+import Head from '../../components/common/Head'
+import Select from 'react-select'
+import AppButton from '../../components/appButton'
 
 const index = () => {
+    const options = [
+        { value: 'partnership', label: 'Partnership' },
+        { value: 'corporation', label: 'Corporation' },
+        { value: 'nonprofit', label: 'Nonprofit Organization' }
+    ]
     return (
         <div>
-            Here
+            <Head title="Contact | Industry of Business" />
+            <ContactTitle title="What's the industry of business?" />
+            <div className="mx-auto py-10" style={{ width: 300 }}>
+                <Select options={options} />
+                <div className="mx-auto w-56 py-10">
+                    <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/contact/industry-of-business' />
+                </div>
+            </div>
+
         </div>
     )
 }
