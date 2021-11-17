7ba74053f1575672dc6f18bf530f5549b8c26e73
nick-andriam <nick@glaxier.com.au>
2021-11-17 23:34:18 +0700

    textarea fixes

--- pages/contact/contact-detail.js
+++ pages/contact/contact-detail.js
@@ -4,11 +4,11 @@ import ContactTitle from '../../components/contact/Title'
 import { withSizeLessThan } from '../../hooks/useWindowSize'
 
 const index = () => {
-    let sm = withSizeLessThan(400)
+    let sm = withSizeLessThan(700)
     return (
         <div className="flex flex-col items-center justify-center">
             <ContactTitle title="Leave Your Contact Detail" />
-            <textarea cols={sm ? 38 : 60} rows={10} placeholder="Message" />
+            <textarea cols={60} rows={10} placeholder="Message" style={{ width: sm ? 300 : 500 }} />
             <div className="mx-auto w-96 flex flex-col items-center justify-center py-9">
                 <AppButton title="Submit" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/' />
             </div>
