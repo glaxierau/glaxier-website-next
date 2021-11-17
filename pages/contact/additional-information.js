aecb16626ab0b5662c929befbdee73d278ff1a26
nick-andriam <nick@glaxier.com.au>
2021-11-17 23:34:18 +0700

    textarea fixes

--- pages/contact/additional-information.js
+++ pages/contact/additional-information.js
@@ -4,11 +4,12 @@ import ContactTitle from '../../components/contact/Title'
 import { withSizeLessThan } from '../../hooks/useWindowSize'
 
 const index = () => {
-    let sm = withSizeLessThan(400)
+    let sm = withSizeLessThan(700)
+    console.log(sm)
     return (
         <div className="flex flex-col items-center justify-center">
             <ContactTitle title="Additional information" />
-            <textarea cols={sm ? 38 : 60} rows={10} placeholder="Message" />
+            <textarea cols={38} rows={10} placeholder="Message" style={{ width: sm ? 300 : 500 }} />
             <div className="mx-auto w-96 flex flex-col items-center justify-center py-9">
                 <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/contact/personal-details' />
                 <Link href="/contact/personal-details">
