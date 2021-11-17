535079de9452074a395d397ca0ee72e59c51ac22
nick-andriam <nick@glaxier.com.au>
2021-11-17 19:22:53 +0700

    expected revenue with slider

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,14 @@ class MyDocument extends Document {
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet" />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    />
                 </Head>
                 <body>
                     <Main />
