ab5ae0f12fdd61511a8824b8cad66968511a1cdb
nick-andriam <nick@glaxier.com.au>
2021-11-17 16:33:02 +0700

    updated svg's

--- components/contact/GoalBox.js
+++ components/contact/GoalBox.js
@@ -4,8 +4,8 @@ import { withSizeLessThan } from '../../hooks/useWindowSize'
 const GoalBox = ({ icon, active = false, name, onClick }) => {
     let sm = withSizeLessThan(600)
     let [boxHovered, setBoxHovered] = useState(false)
-    let link = boxHovered ? `/assets/img/contact/icons/white_icons/${icon}.png` : `/assets/img/contact/icons/blue_icons/${icon}.png`
-    let activeLink = `/assets/img/contact/icons/white_icons/${icon}.png`
+    let link = boxHovered ? `/assets/img/contact/icons/white_icons/${icon}.svg` : `/assets/img/contact/icons/blue_icons/${icon}.svg`
+    let activeLink = `/assets/img/contact/icons/white_icons/${icon}.svg`
     let basicClassName = "lg:w-32 lg:h-32 md:w-32 md:h-32 w-24 h-24 flex flex-col items-center justify-center cursor-pointer m-2 rounded-lg"
     let activeClassName = `bg-red text-white border-0`
     let inactiveClassName = `bg-white-dark text-purple border border-purple`
