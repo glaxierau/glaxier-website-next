906806d58d21af551a8006ba3ad9e4bd2b000dc0
nick-andriam <nick@glaxier.com.au>
2021-11-17 19:22:53 +0700

    expected revenue with slider

--- pages/contact/expected-revenue.js
+++ pages/contact/expected-revenue.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import ContactTitle from '../../components/contact/Title'
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import AppButton from '../../components/appButton';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors'
+import { withSizeLessThan } from '../../hooks/useWindowSize';
+
+
+function valuetext(value) {
+    return `${value}$`;
+}
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#9FB0E4",
+        },
+    },
+});
+
 
 const Revenue = () => {
+    const sm = withSizeLessThan(720)
     return (
         <div>
-            industry
+            <ContactTitle title="What's your expected revenue from online channels?" />
+            <div className="relative h-40 mx-auto" style={{ width: sm ? 300 : 600 }}>
+                <div className="absolute -left-5 top-0 flex flex-col z-0 justify-center items-center">
+                    <p className="text-red font-bold py-3">50,0000</p>
+                    <img src="/assets/img/contact/icons/blue_icons/expected_revenue.svg" alt="revenue" width={40} />
+                </div>
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-2/4 flex items-center justify-center mx-auto">
+                    <ThemeProvider theme={theme}>
+                        <Box sx={{ width: sm ? 300 : 600 }}>
+                            <Slider
+                                aria-label="Price"
+                                defaultValue={50000}
+                                getAriaValueText={valuetext}
+                                valueLabelDisplay="auto"
+                                step={1000000}
+                                marks
+                                min={50000}
+                                max={10000000}
+                                color="primary"
+                            />
+                        </Box>
+                    </ThemeProvider>
+                </div>
+                <div className="absolute -right-5 top-0 z-0 flex flex-col justify-center items-center">
+                    <p className="text-red font-bold py-3">10,000,000</p>
+                    <img src="/assets/img/contact/icons/blue_icons/expected_revenue.svg" alt="revenue" width={40} />
+                </div>
+            </div>
+            <div className="mx-auto w-56 py-10">
+                <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/contact/services' />
+            </div>
         </div>
     )
 }
--- pages/contact/industry-of-business.js
+++ pages/contact/industry-of-business.js
@@ -5,6 +5,7 @@ import Select from 'react-select'
 import AppButton from '../../components/appButton'
 
 const index = () => {
+
     const options = [
         { value: 'partnership', label: 'Partnership' },
         { value: 'corporation', label: 'Corporation' },
@@ -13,19 +14,24 @@ const index = () => {
     const customStyles = {
         option: (provided, state) => ({
             ...provided,
-            borderBottom: '1px dotted pink',
-            color: state.isSelected ? 'red' : 'blue',
+            backgroundColor: state.isFocused ? "#9FB0E4" : 'white',
+            color: state.isFocused ? 'white' : '#9FB0E4',
             padding: 20,
         }),
         control: () => ({
-            // none of react-select's styles are passed to <Control />
+            display: 'flex',
             width: 400,
+            borderRadius: 100,
+            padding: 4,
+            border: '1px solid #9FB0E4',
+
         }),
         singleValue: (provided, state) => {
             const opacity = state.isDisabled ? 0.5 : 1;
             const transition = 'opacity 300ms';
+            const color = '#9FB0E4'
 
-            return { ...provided, opacity, transition };
+            return { ...provided, opacity, transition, color };
         }
     }
 
@@ -33,7 +39,7 @@ const index = () => {
         <div>
             <Head title="Contact | Industry of Business" />
             <ContactTitle title="What's the industry of business?" />
-            <div className="mx-auto py-10 bg-gray-400" style={{ width: 500 }}>
+            <div className="mx-auto py-10" style={{ width: 400 }} >
                 <Select options={options} styles={customStyles} />
                 <div className="mx-auto w-56 py-10">
                     <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/contact/expected-revenue' />
