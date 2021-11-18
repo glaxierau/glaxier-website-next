446a8582fd41a7d582219697f6767132dee1be85
nick-andriam <nick@glaxier.com.au>
2021-11-18 15:13:37 +0700

    sidebar animation

--- components/layouts/SideBar.js
+++ components/layouts/SideBar.js
@@ -16,21 +16,26 @@ const SideBar = ({ open, setNav }) => {
 
     return (
         <>
-            <motion.div className="relative overflow-y-hidden bg-white"
-                initial={{ opacity: 0 }}
-                animate={{ y: open ? 0 : -2, opacity: open ? 1 : 0, height: open ? '100vh' : '0vh' }}
+            <motion.div className={` w-screen h-screen absolute top-14 left-0 z-0`}
+                initial={{ opacity: 0, borderRadius: '0px 0px 40px 40px' }}
+                animate={{ opacity: 1, height: open ? '100vh' : '0vh' }}
                 transition={{ stiffness: 100, duration: 0.2, staggerChildren: 0.5, delayChildren: 0.4 }}>
-                <img src="/assets/svg/shape.svg" />
-                <div className="h-screen w-screen bg-white-dark py-2 px-5">
-                    <div>
-                        <LanguageSection />
-                        <br />
-                        <br />
-                        <NavList navs={navs} setNav={setNav} />
-                        <div style={{ height: '1px' }} className="my-10 mx-auto w-full bg-purple" />
-                        <SearchBox />
+                <motion.div className="relative overflow-y-hidden bg-white"
+                    initial={{ opacity: 0 }}
+                    animate={{ y: open ? 0 : -2, opacity: open ? 1 : 0, height: open ? '100vh' : '0vh' }}
+                    transition={{ stiffness: 100, duration: 0.2, staggerChildren: 0.5, delayChildren: 0.4 }}>
+                    <img src="/assets/svg/shape.svg" />
+                    <div className="h-screen w-screen bg-white-dark py-2 px-5">
+                        <div>
+                            <LanguageSection />
+                            <br />
+                            <br />
+                            <NavList navs={navs} setNav={setNav} />
+                            <div style={{ height: '1px' }} className="my-10 mx-auto w-full bg-purple" />
+                            <SearchBox />
+                        </div>
                     </div>
-                </div>
+                </motion.div>
             </motion.div>
         </>
     )
