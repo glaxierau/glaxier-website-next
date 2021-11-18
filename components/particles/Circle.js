a16bcda562e5fbe9d4ab84a0385c42a03a94812f
nick-andriam <nick@glaxier.com.au>
2021-11-18 10:40:28 +0700

    services in home

--- components/particles/Circle.js
+++ components/particles/Circle.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Circle = ({ title, ...otherProps }) => {
+const Circle = ({ active, title, onClick, ...otherProps }) => {
+    const basicClassName = `absolute z-20 flex items-center justify-center lg:w-56 lg:h-56 w-32 h-32 rounded-full cursor-pointer ${active && 'border-2'} hover:border-2`
     return (
-        <div id="circle" className="circle absolute z-20 flex items-center justify-center lg:w-56 lg:h-56 w-32 h-32 rounded-full hover:border-2 cursor-pointer" {...otherProps}>
-            <div id="circle-shadow" className="lg:w-48 lg:h-48 w-28 h-28 rounded-full bg-white bg-opacity-95 hover:bg-red flex items-center justify-center px-10 shadow-around hover:shadow-none text-red hover:text-white transition duration-200 ease-in-out">
+        <div onClick={onClick} className={`${basicClassName}`} {...otherProps}>
+            <div onClick={onClick} className={`lg:w-48 lg:h-48 w-28 h-28 rounded-full bg-white ${active && 'bg-red'}  bg-opacity-95 hover:bg-red flex items-center justify-center px-10 ${active ? 'shadow-none' : 'shadow-around'} hover:shadow-none ${active ? 'text-white' : 'text-red'} hover:text-white transition duration-200 ease-in-out`}>
                 <h2 className="lg:text-xl text-sm text-center font-bold">{title}</h2>
             </div>
         </div>
--- components/service/Service.js
+++ components/service/Service.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../title'
-import style from '../../styles/Service.module.css'
 import { mobileScreen } from '../../hooks/useWindowSize'
 import Particles from '../particles/particles'
 import Circle from '../particles/Circle'
+import { setToTrue } from '../../hooks/setToTrue'
+import { setAllToFalse } from '../../hooks/setAllToFalse'
 
 const Service = () => {
     let sm = mobileScreen()
+    const [lists, setLists] = useState([
+        { type: 1, label: "Graphic Design", active: false },
+        { type: 2, label: "Digital Advertising", active: false },
+        { type: 3, label: "Social Media Management", active: false },
+        { type: 4, label: "Website Development", active: false },
+    ])
+
+    const onSelecting = (e) => {
+        const innerText = e.target.innerText
+        const foundIndex = lists.filter(list => list.label === innerText)
+        setAllToFalse(lists, setLists)
+        setToTrue(foundIndex[0], lists, setLists)
+    }
     return (
         <div className="bg-white-dark pt-10">
             <div className="lg:px-44 px-4">
@@ -17,10 +31,10 @@ const Service = () => {
             <div className="w-full h-screen mt-20 overflow-hidden flex justify-center items-center relative text-center">
                 <img src="/assets/img/home/Artboard2.png" className={"circle_image absolute top-0 z-10 w-screen object-cover h-screen"} alt="circles" />
                 <Particles />
-                <Circle style={sm ? { top: '8rem', left: '5%' } : { top: '5rem', left: '20%' }} title="Graphic Design" />
-                <Circle style={sm ? { top: '5rem', right: '5%' } : { top: '5rem', right: '13%' }} title="Digital Advertising" />
-                <Circle style={sm ? { bottom: '8rem', left: '5%' } : { bottom: '2rem', left: '14%' }} title="Social Media Management" />
-                <Circle style={sm ? { bottom: '8rem', right: '5%' } : { bottom: '2rem', right: '20%' }} title="Website Development" />
+                <Circle style={sm ? { top: '8rem', left: '5%' } : { top: '5rem', left: '20%' }} title={lists[0].label} onClick={(e) => onSelecting(e)} active={lists[0].active} />
+                <Circle style={sm ? { top: '5rem', right: '5%' } : { top: '5rem', right: '13%' }} title={lists[1].label} onClick={(e) => onSelecting(e)} active={lists[1].active} />
+                <Circle style={sm ? { bottom: '8rem', left: '5%' } : { bottom: '2rem', left: '14%' }} title={lists[2].label} onClick={(e) => onSelecting(e)} active={lists[2].active} />
+                <Circle style={sm ? { bottom: '8rem', right: '5%' } : { bottom: '2rem', right: '20%' }} title={lists[3].label} onClick={(e) => onSelecting(e)} active={lists[3].active} />
                 <div className="position top-0 left-1/2 z-10 flex items-center justify-center flex-col mt-6">
                     <Title title="Our Services" lineColor="#fff" lineWidth="210" /> <br />
                     <h3 className="lg:text-lg font-black text-base">We can do these awesome things</h3> <br />
