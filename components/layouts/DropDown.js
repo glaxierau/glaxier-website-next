f7ec6ff3d5258728b66603a08b92e7e1245ce406
nick-andriam <nick@glaxier.com.au>
2021-11-22 12:43:56 +0700

    dropdown arrow for desktop

--- components/layouts/DropDown.js
+++ components/layouts/DropDown.js
@@ -4,17 +4,19 @@ import { motion } from 'framer-motion'
 const DropDown = ({ open, onHover, onLeave, position, dropDownList, id }) => {
     const DropDownCard = ({ label, list }) => {
         return (
-            <Link href={list.to}>
-                <motion.div id="dropdown" className="relative hover:bg-blue-dark text-blue-dark hover:text-white py-3 px-8 " style={{ width: 200 }}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.8 }}
-                    onPointerEnter={onHover}
-                    onPointerLeave={onLeave}
-                >
-                    <p className="text-center" style={{ fontSize: '0.8rem' }}>{label}</p>
-                </motion.div>
-            </Link>
+            <>
+                <Link href={list.to}>
+                    <motion.div id="dropdown" className="relative hover:bg-blue-dark text-blue-dark hover:text-white py-3 px-8 " style={{ width: 200 }}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.8 }}
+                        onPointerEnter={onHover}
+                        onPointerLeave={onLeave}
+                    >
+                        <p className="text-center" style={{ fontSize: '0.8rem' }}>{label}</p>
+                    </motion.div>
+                </Link>
+            </>
         )
     }
     return (
