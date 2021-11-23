e24791409a64bd4018ec5fd33b8c1adba900cca3
nick-andriam <nick@glaxier.com.au>
2021-11-23 18:15:47 +0700

    blog hover

--- components/articles/ArticleCard.js
+++ components/articles/ArticleCard.js
@@ -1,21 +1,25 @@
+import { motion } from 'framer-motion'
 import Link from 'next/link'
+import { useState } from 'react'
 
 const ArticleCard = ({ id, title, category, img, date }) => {
-
+    // let [isDragged, setIsDragged] = useState(false)
+    // let href = '#'
     let href = `/blog/[category]/[id]`
+    // let asHref = "#"
     let asHref = `/blog/${category}/${id}`
+    // console.log(isDragged)
     return (
         <>
-            <Link href={href} as={asHref}>
-                <div className=" bg-white shadow-sm lg:m-6 m-2 cursor-pointer hover:shadow-around">
-                    {/* <div className="h-lg lg:w-lg w-80 bg-white shadow-sm lg:m-4 m-2"> */}
+            <Link href={href} as={asHref} className="pointer-events-none">
+                <motion.div whileHover={{ scale: 1 }} initial={{ scale: 0.98 }} className=" bg-white shadow-around lg:m-6 m-2 cursor-pointer">
                     {/* image  */}
                     <div className="article_image" style={{ backgroundImage: `url(${img})` }} />
                     {/* meta */}
                     <p className="p-4 text-black-light">{date}</p>
                     {/* title  */}
                     <p className="p-4 text-red font-bold ">{title}</p>
-                </div>
+                </motion.div>
             </Link>
             <style jsx>
                 {`
