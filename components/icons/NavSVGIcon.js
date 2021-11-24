6ea52899dccff86606ccda90824b60b460dcd8b9
nick-andriam <nick@glaxier.com.au>
2021-11-24 15:31:57 +0700

    language choice

--- components/icons/NavSVGIcon.js
+++ components/icons/NavSVGIcon.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import DropDown from '../layouts/DropDown'
+import { mobileScreen } from '../../hooks/useWindowSize'
 
-function NavSVGIcon({ size = ['20', '20'], children, otherProps }) {
+function NavSVGIcon({ size = ['20', '20'], children, withDropDown = false, ...otherProps }) {
+    let lang = [
+        { label: 'EN' },
+        { label: 'TH' },
+    ]
+    const lg = !mobileScreen()
+    const [open, setOpen] = useState(false)
+    const [position, setPosition] = useState(0)
+    const getPosition = () => {
+        const value = document.getElementById("globeSVG").offsetLeft
+        setPosition(value - 20)
+    }
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" className="fill-purple hover:fill-red cursor-pointer" width={size[0]} height={size[1]} viewBox="0 0 29.257 29.25" {...otherProps}>
-            {children}
-        </svg>
+        <div id="globeSVG"
+            className="h-20 w-10 flex items-center justify-start"
+            onMouseEnter={() => { setOpen(true), getPosition() }}
+            onMouseLeave={() => setOpen(false)}
+            onClick={() => { setOpen(!open), getPosition() }}>
+            <motion.svg xmlns="http://www.w3.org/2000/svg" className={`fill-purple ${lg && 'hover:fill-red'} cursor-pointer`} width={size[0]} height={size[1]} viewBox="0 0 29.257 29.25" {...otherProps}
+                onClick={() => setOpen(!open)}
+            >
+                {children}
+            </motion.svg>
+            {withDropDown && <DropDown dropDownList={lang} position={0} open={open} width={20} position={position} onLeave={() => setOpen(false)} />}
+        </div>
     )
 }
 
