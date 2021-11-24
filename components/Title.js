d5d19e4673261e750c48d3487957575689a3e801
nick-andriam <nick@glaxier.com.au>
2021-11-24 12:28:14 +0700

    toggle circles in services

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,12 @@
+import { motion } from 'framer-motion'
 import CurvyLine from './line/CurvyLine'
 
 const Title = ({ title, lineColor = "#9FB0E483", lineWidth, height }) => {
     return (
-        <div className="flex flex-col justify-center items-center">
+        <motion.div className="flex flex-col justify-center items-center">
             <CurvyLine color={lineColor} width={lineWidth} height={height} />
             <h2 className="text-3xl font-extrabold">{title}</h2>
-        </div>
+        </motion.div>
     )
 }
 
