ecde3a91748b98a9382ba78cf7b909aa2e3c4ce1
nick-andriam <nick@glaxier.com.au>
2021-11-29 18:19:35 +0700

    fixed language prefix and bug

--- components/layouts/SideBar.js
+++ components/layouts/SideBar.js
@@ -11,14 +11,14 @@ const SideBar = ({ open, setNav }) => {
         <>
             <motion.div className={` w-screen absolute top-14 left-0 z-0`}
                 initial={{ opacity: 0, borderRadius: '0px 0px 40px 40px' }}
-                animate={{ opacity: 1, height: open ? 'auto' : '0vh' }}
+                animate={{ opacity: 1, height: open ? 'auto' : '0vh', display: open ? 'block' : 'none' }}
                 transition={{ stiffness: 100, duration: 0.2, staggerChildren: 0.5, delayChildren: 0.4 }}>
                 <motion.div className=" bg-white"
                     initial={{ opacity: 0 }}
                     animate={{ y: open ? 0 : -2, opacity: open ? 1 : 0, height: open ? '100vh' : '0vh' }}
                     transition={{ stiffness: 100, duration: 0.2, staggerChildren: 0.5, delayChildren: 0.4 }}>
                     <img src="/assets/svg/shape.svg" />
-                    <div className="w-screen bg-white-dark h-auto py-2 px-5 overflow-y-scroll">
+                    <div className="w-screen bg-white-dark py-2 px-5">
                         <LanguageSection />
                         <br />
                         <br />
