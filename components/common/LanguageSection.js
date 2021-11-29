6f8ec62ec638bde5b1089df7f6a45b5ff171e1db
nick-andriam <nick@glaxier.com.au>
2021-11-29 15:04:58 +0700

    fixed language prefix

--- components/common/LanguageSection.js
+++ components/common/LanguageSection.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { getSpecificPart } from '../../hooks/getSpecificPart'
 
 const LanguageSection = () => {
     const router = useRouter()
     const lang = router.locales
-
     return (
         <div className="flex">
             {lang.map(language => (
                 <Link key={language} href={router.asPath} locale={language}>
-                    <p className={`mr-4 text-base cursor-pointer font-light text-purple`}>{language}</p>
+                    <p className={`mr-4 text-base cursor-pointer font-light text-purple hover:text-red`}>{getSpecificPart(language, 1)}</p>
                 </Link>
             ))}
         </div>
