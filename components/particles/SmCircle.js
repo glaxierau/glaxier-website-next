f2e0130f71b64261b5370f655f1c7b7cbe46901c
nick-andriam <nick@glaxier.com.au>
2021-11-29 19:30:46 +0700

    scroll behavior fix

--- components/particles/SmCircle.js
+++ components/particles/SmCircle.js
@@ -1,8 +1,22 @@
+import { motion } from "framer-motion"
+import { getRandom, secondRandom } from "../../hooks/randNum"
 
 const SmCircle = ({ size, ...otherProps }) => {
+    const floatingVariants = {
+        animate: {
+            y: [0, getRandom(2), getRandom(10), getRandom(-5), 0],
+            x: [0, getRandom(1), getRandom(2), getRandom(1), 0],
+        },
+    }
     return (
-        <div className={`absolute z-20 bg-white rounded-full h-${size} w-${size}`} {...otherProps}>
-        </div>
+        <motion.div
+            className={`absolute z-20 bg-white rounded-full h-${size} w-${size}`} {...otherProps}
+            variants={floatingVariants}
+            animate="animate"
+            transition={{ duration: 4, delay: secondRandom(4), repeat: Infinity }}
+
+        >
+        </motion.div>
     )
 }
 
