81a777f9ad2c235aa10b6944252aab56aec562c3
nick-andriam <nick@glaxier.com.au>
2021-12-14 15:44:26 +0700

    minor updates

--- components/Title.js
+++ components/Title.js
@@ -3,9 +3,9 @@ import CurvyLine from './line/CurvyLine'
 
 const Title = ({ title, lineColor = "#9FB0E483", lineWidth, height }) => {
     return (
-        <motion.div className="flex flex-col justify-center items-center">
+        <motion.div className="relative flex flex-col justify-center items-center">
             <CurvyLine color={lineColor} width={lineWidth} height={height} />
-            <h2 className="lg:text-3xl md:text-2xl text-2xl font-extrabold">{title}</h2>
+            <h2 className="lg:text-3xl md:text-2xl text-2xl font-extrabold text-center z-10">{title}</h2>
         </motion.div>
     )
 }
