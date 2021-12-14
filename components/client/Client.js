f297196627136b68af82ace95f9c1e50a73c584b
nick-andriam <nick@glaxier.com.au>
2021-12-14 15:07:29 +0700

    our client update

--- components/client/Client.js
+++ components/client/Client.js
@@ -1,15 +1,19 @@
+import Slider from 'react-slick'
+import { client_settings } from '../../config/carousel.setting'
 import Badge from '../icons/Badge'
 import Title from '../Title'
 
 const Client = ({ clientTitle, clientDescription, clients }) => {
     return (
-        <div className="relative flex flex-col items-center justify-center mt-2 bg-white-dark lg:h-screen lg:py-0 py-16">
-            <Title title={clientTitle} lineColor="#CFD7F1" lineWidth="180" /> <br /> <br />
-            <p className="lg:w-2/6 w-full xl:px-0 lg:px-0 md:px-36 px-5 text-black-light">{clientDescription[0].children[0].text}</p>
-            <div className="flex lg:mt-20 mt-10 lg:w-3/5 w-4/5 items-center justify-between flex-wrap">
+        <div className="bg-white-dark mt-2 py-20">
+            <div className="relative flex flex-col items-center justify-center">
+                <Title title={clientTitle} lineColor="#CFD7F1" lineWidth="180" /> <br /> <br />
+                <p className="lg:w-2/6 w-full xl:px-0 lg:px-0 md:px-36 px-5 text-black-light">{clientDescription[0].children[0].text}</p>
 
-                {clients.map(client => <Badge key={client._key} />)}
             </div>
+            <Slider {...client_settings} className="lg:w-3/5 w-10/12 mx-auto mt-14" >
+                {clients.map(client => <Badge key={client._key} />)}
+            </Slider>
         </div>
     )
 }
