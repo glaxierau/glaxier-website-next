ba81aeae31d0b050d7e031fc260c30a2aae0936f
nick-andriam <nick@glaxier.com.au>
2021-12-23 16:36:17 +0700

    home, about, service using sanity

--- components/contact/GoalBox.js
+++ components/contact/GoalBox.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import { withSizeLessThan } from '../../hooks/useWindowSize'
+import Img from 'next/image'
 
 const GoalBox = ({ icon, active = false, name, onClick }) => {
     let sm = withSizeLessThan(600)
     let [boxHovered, setBoxHovered] = useState(false)
-    let link = boxHovered ? `/assets/img/contact/icons/white_icons/${icon}.svg` : `/assets/img/contact/icons/blue_icons/${icon}.svg`
-    let activeLink = `/assets/img/contact/icons/white_icons/${icon}.svg`
     let basicClassName = "lg:w-32 lg:h-32 md:w-32 md:h-32 w-24 h-24 flex flex-col items-center justify-center cursor-pointer m-2 rounded-lg transition duration-100 ease-in-out"
     let activeClassName = `bg-red text-white border-0`
     let inactiveClassName = `bg-white-dark text-black border border-purple`
@@ -17,7 +16,7 @@ const GoalBox = ({ icon, active = false, name, onClick }) => {
                 onMouseLeave={() => setBoxHovered(false)}
                 onClick={onClick}
             >
-                <img src={active && activeLink || link} width={sm ? 30 : 50} />
+                <Img {...icon} width={sm ? 30 : 50} height={sm ? 30 : 50} />
                 <p className={`py-2 text-center`} style={{ fontSize: '0.8rem' }}>{name}</p>
             </div>
         </>
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Goals from './goals'
 import Layout from './layout'
-import { useSelector } from 'react-redux'
+import { getData } from '../../hooks/getData'
 
-
-
-const Contact = () => {
-    const { data } = useSelector(state => state.data)
-    const { contact } = data
-    if (contact) {
+const Contact = (props) => {
+    if (props) {
         return (
-            <Layout metadata={contact.pageInfo.metadata}>
+            <Layout metadata={props.pageInfo.metadata} {...props}>
                 <div className="h-20" />
-                <Goals />
+                <Goals {...props.interactiveForm} />
             </Layout>
         )
+    }
+}
+
+export const getServerSideProps = async (req, res) => {
+
+    let lang = req.locale
+    let language = await getData(`*[_type == 'languageOption' && language == '${lang}']{_id}[0]`)
+    language = language._id
+
+    const data = await getData(
+        `*[ _type == 'contact' && pageInfo.lang._ref == '107fa697-d70e-46bb-9d5a-5d8952bafd3a'][0]{
+            ...,
+            "interactiveForm":*[ _type == 'interactiveForm'][1]
+            }`)
+    return {
+        props: data
     }
 }
 
