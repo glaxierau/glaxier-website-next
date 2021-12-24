362ffba1f57666f093f05436f09e56862b4feffd
nick-andriam <nick@glaxier.com.au>
2021-12-24 10:12:32 +0700

    scroll animation

--- components/particles/Circle.js
+++ components/particles/Circle.js
@@ -8,13 +8,16 @@ const Circle = ({ active, title, onClick, ...otherProps }) => {
 
     const floatingVariants = {
         initial: {
-            y: 0
+            y: 0,
+            x: 0
         },
         animate: {
-            y: [0, getRandom(-2), getRandom(5), getRandom(-5), 0],
+            y: [0, getRandom(-2), getRandom(10), getRandom(-5), 0],
+            x: [0, getRandom(-2), getRandom(5), getRandom(-5), 0]
         },
         exit: {
-            y: 0
+            y: 0,
+            x: 0
         }
     }
     return (
@@ -23,7 +26,7 @@ const Circle = ({ active, title, onClick, ...otherProps }) => {
             initial="initial"
             animate="animate"
             exit="exit"
-            transition={{ duration: 4, delay: secondRandom(4), repeat: Infinity }}
+            transition={{ duration: 10, delay: secondRandom(4), repeat: Infinity }}
         >
             <div onClick={onClick} className={`lg:w-48 lg:h-48 md:w-36 md:h-36 w-28 h-28 rounded-full bg-white ${active && 'bg-red'}  bg-opacity-95 hover:bg-red flex items-center justify-center px-10 ${active ? 'shadow-none' : 'shadow-around'} hover:shadow-none ${active ? 'text-white' : 'text-red'} hover:text-white transition duration-200 ease-in-out`}>
                 <h2 className="lg:text-xl text-sm text-center font-bold">{title}</h2>
