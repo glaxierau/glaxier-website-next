54045f8063137e739ab6bd098a363aa269dc86a4
Tan <thawin999@gmail.com>
2022-01-07 10:31:57 +1000

    added prettier and eslint plugin

--- pages/contact/contact-details.js
+++ pages/contact/contact-details.js
@@ -12,39 +12,46 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { getSpecificForm } from '../../hooks/form'
 
-
-
-
-const index = ({ personalDetailSection }) => {
+function Index({ personalDetailSection }) {
     const type = 'Contact Detail'
-    const { form } = useSelector(s => s.contactForm)
-    const index = form.findIndex(i => i.type === type)
+    const { form } = useSelector((s) => s.contactForm)
+    const index = form.findIndex((i) => i.type === type)
     const _form = form[index]
     const postUrl = process.env.NEXT_APP_POST_URL
     const [checkbox, setCheck] = useState(true)
     const router = useRouter()
-    const label = "I agree to receive emails from Glaxier containing offers and developments that Glaxier thinks may interest me."
-
+    const label =
+        'I agree to receive emails from Glaxier including news and developments.'
 
     const pushDataLayer = () => {
-        pushDataLayerForm('Contact Us', type, _form.firstName, _form.lastName, _form.email, _form.phoneNumber)
+        pushDataLayerForm(
+            'Contact Us',
+            type,
+            _form.firstName,
+            _form.lastName,
+            _form.email,
+            _form.phoneNumber
+        )
     }
-
 
     const onGettingInput = (e) => {
         const value = e.target.value
         const name = e.target.name
         switch (name) {
-            case 'MERGE2': return form[index].lastName = value;
-            case 'MERGE1': return form[index].firstName = value;
-            case 'MERGE0': return form[index].email = value;
-            case 'MERGE4': return form[index].phoneNumber = value;
-            default: return ''
+            case 'MERGE2':
+                return (form[index].lastName = value)
+            case 'MERGE1':
+                return (form[index].firstName = value)
+            case 'MERGE0':
+                return (form[index].email = value)
+            case 'MERGE4':
+                return (form[index].phoneNumber = value)
+            default:
+                return ''
         }
     }
 
     const Form = () => {
-
         const onSubmittingForm = async (e) => {
             e.preventDefault()
             pushDataLayer()
@@ -54,7 +61,7 @@ const index = ({ personalDetailSection }) => {
                 email: _form.email,
                 phone: _form.phoneNumber,
                 goal: getSpecificForm(form, 'Goal').value,
-                status: 'subscribed'
+                status: 'subscribed',
             }
 
             // check subscription permissions
@@ -65,7 +72,8 @@ const index = ({ personalDetailSection }) => {
             try {
                 const response = await axios.put('/api/audiences/add', content)
                 const resStatus = response.data.status
-                if (resStatus === 'subscribed' || resStatus === 'unsubscribed') router.push('/contact/industry-of-business')
+                if (resStatus === 'subscribed' || resStatus === 'unsubscribed')
+                    router.push('/contact/industry-of-business')
                 else throw JSON.parse(response.data.response.text).title
             } catch (err) {
                 alert(err)
@@ -74,7 +82,10 @@ const index = ({ personalDetailSection }) => {
 
         return (
             <>
-                <form className="native_input flex flex-col justify-center items-center" onSubmit={(e) => onSubmittingForm(e)} >
+                <form
+                    className="native_input flex flex-col justify-center items-center"
+                    onSubmit={(e) => onSubmittingForm(e)}
+                >
                     <input
                         placeholder={personalDetailSection.firstNamePlaceholder}
                         defaultValue={form[index].firstName}
@@ -82,21 +93,66 @@ const index = ({ personalDetailSection }) => {
                         name="MERGE1"
                         id="MERGE1"
                         className="lg:w-96 md:w-80 w-80 pl-4"
-                        onChange={e => onGettingInput(e)}
+                        onChange={(e) => onGettingInput(e)}
                         required
                     />
-                    <input placeholder={personalDetailSection.lastNamePlaceholder} defaultValue={form[index].lastName} type="text" name="MERGE2" id="MERGE2" className="lg:w-96 md:w-80 w-80 pl-4" onChange={e => onGettingInput(e)} required />
-                    <input placeholder={personalDetailSection.emailPlaceholder} defaultValue={form[index].email} type="email" name="MERGE0" id="MERGE0" className="lg:w-96 md:w-80 w-80 pl-4" onChange={e => onGettingInput(e)} required />
-                    <input placeholder={personalDetailSection.numberPlaceholder} defaultValue={form[index].phoneNumber} type="tel" name="MERGE4" id="MERGE4" className="lg:w-96 md:w-80 w-80 pl-4" onChange={e => onGettingInput(e)} required />
-                    <div className='flex justify-start items-center lg:w-96 md:w-80 w-80 py-2'>
-                        <CheckBox checked={checkbox} sx={{ color: '#9FB0E4', '&.Mui-checked': { color: '#9FB0E4' } }} onChange={e => setCheck(e.target.checked)} />
-                        <p className='text-xss leading-3 text-gray-400'>{label}</p>
+                    <input
+                        placeholder={personalDetailSection.lastNamePlaceholder}
+                        defaultValue={form[index].lastName}
+                        type="text"
+                        name="MERGE2"
+                        id="MERGE2"
+                        className="lg:w-96 md:w-80 w-80 pl-4"
+                        onChange={(e) => onGettingInput(e)}
+                        required
+                    />
+                    <input
+                        placeholder={personalDetailSection.emailPlaceholder}
+                        defaultValue={form[index].email}
+                        type="email"
+                        name="MERGE0"
+                        id="MERGE0"
+                        className="lg:w-96 md:w-80 w-80 pl-4"
+                        onChange={(e) => onGettingInput(e)}
+                        required
+                    />
+                    <input
+                        placeholder={personalDetailSection.numberPlaceholder}
+                        defaultValue={form[index].phoneNumber}
+                        type="tel"
+                        name="MERGE4"
+                        id="MERGE4"
+                        className="lg:w-96 md:w-80 w-80 pl-4"
+                        onChange={(e) => onGettingInput(e)}
+                        required
+                    />
+                    <div className="flex justify-start items-center lg:w-96 md:w-80 w-80 py-2">
+                        <CheckBox
+                            checked={checkbox}
+                            sx={{
+                                color: '#9FB0E4',
+                                '&.Mui-checked': { color: '#9FB0E4' },
+                            }}
+                            onChange={(e) => setCheck(e.target.checked)}
+                        />
+                        <p className="text-xss leading-3 text-gray-400">
+                            {label}
+                        </p>
                     </div>
                     <div className="mx-auto w-96 flex flex-col items-center justify-center">
-                        <AppButton title="Continue" width={200} type='submit' bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" />
+                        <AppButton
+                            title="Continue"
+                            width={200}
+                            type="submit"
+                            bgColor="bg-blue-dark"
+                            bgColorHover="hover:bg-red"
+                            txtColor="text-white"
+                        />
                     </div>
-                    <Link href="/contact/industry-of-business">
-                        <p className="cursor-pointer underline text-purple hover:text-red pb-5">SKIP</p>
+                    <Link href="/contact/industry-of-business" passHref={true}>
+                        <p className="cursor-pointer underline text-purple hover:text-red pb-5">
+                            SKIP
+                        </p>
                     </Link>
                 </form>
             </>
@@ -111,8 +167,6 @@ const index = ({ personalDetailSection }) => {
     )
 }
 
-
-
 export const getStaticProps = async (ctx) => {
     const locale = ctx.locale
     let index = 1
@@ -121,7 +175,9 @@ export const getStaticProps = async (ctx) => {
     } else {
         index = 0
     }
-    const props = await getData(`*[_type == 'interactiveForm'][${index}]{personalDetailSection}`)
+    const props = await getData(
+        `*[_type == 'interactiveForm'][${index}]{personalDetailSection}`
+    )
     return { props }
 }
 
