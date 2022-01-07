6f40b688d98655fffb8f773cac289abe728f236c
Tan <thawin999@gmail.com>
2022-01-07 10:31:57 +1000

    added prettier and eslint plugin

--- pages/contact/additional-information.js
+++ pages/contact/additional-information.js
@@ -9,17 +9,16 @@ import { getData } from '../../hooks/getData'
 import { withSizeLessThan } from '../../hooks/useWindowSize'
 import axios from 'axios'
 
-
-const index = ({ additonalInfoSection }) => {
+function Index({ additonalInfoSection }) {
     const type = 'Additional Information'
     const dispatch = useDispatch()
     const router = useRouter()
-    const { form } = useSelector(s => s.contactForm)
-    const index = form.findIndex(i => i.type === type)
+    const { form } = useSelector((s) => s.contactForm)
+    const index = form.findIndex((i) => i.type === type)
     let sm = withSizeLessThan(700)
 
     const getSpecificForm = (array, type) => {
-        const value = array.filter(v => v.type === type)[0]
+        const value = array.filter((v) => v.type === type)[0]
         return value
     }
 
@@ -36,10 +35,12 @@ const index = ({ additonalInfoSection }) => {
         try {
             const response = await axios.patch('/api/audiences/update', content)
             const resStatus = response.data.status
-            if (resStatus === 'subscribed' || resStatus === 'unsubscribed') { alert('Submitted successfully!'), router.push('/') }
-            else throw JSON.parse(response.data.response.text).title
-        } catch (err) { throw err }
-
+            if (resStatus === 'subscribed' || resStatus === 'unsubscribed') {
+                alert('Submitted successfully!'), router.push('/')
+            } else throw JSON.parse(response.data.response.text).title
+        } catch (err) {
+            throw err
+        }
     }
 
     const onGettingInfo = (e) => {
@@ -51,18 +52,30 @@ const index = ({ additonalInfoSection }) => {
         <div className="flex flex-col items-center justify-center">
             <SectionHead title="Contact Us | Contact Detail" />
             <ContactTitle title={additonalInfoSection.question} />
-            <textarea value={form[index].value || ''} cols={60} rows={10} placeholder={additonalInfoSection.placeholder} style={{ width: sm ? 300 : 500 }}
-                onChange={(e) => onGettingInfo(e)} />
+            <textarea
+                value={form[index].value || ''}
+                cols={60}
+                rows={10}
+                placeholder={additonalInfoSection.placeholder}
+                style={{ width: sm ? 300 : 500 }}
+                onChange={(e) => onGettingInfo(e)}
+            />
             <div className="mx-auto w-96 flex flex-col items-center justify-center py-9">
-                <AppButton title="Submit" type="submit" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" clicked={() => onSubmit()} />
+                <AppButton
+                    title="Submit"
+                    type="submit"
+                    width={200}
+                    bgColor="bg-blue-dark"
+                    bgColorHover="hover:bg-red"
+                    txtColor="text-white"
+                    clicked={() => onSubmit()}
+                />
             </div>
         </div>
     )
 }
 
-export default index
-
-
+export default Index
 
 export const getStaticProps = async (ctx) => {
     const locale = ctx.locale
@@ -72,8 +85,10 @@ export const getStaticProps = async (ctx) => {
     } else {
         index = 0
     }
-    const props = await getData(`*[_type == 'interactiveForm'][${index}]{additonalInfoSection}`)
+    const props = await getData(
+        `*[_type == 'interactiveForm'][${index}]{additonalInfoSection}`
+    )
     return {
-        props
+        props,
     }
 }
--- pages/contact/expected-revenue.js
+++ pages/contact/expected-revenue.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import ContactTitle from '../../components/contact/Title'
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
-import AppButton from '../../components/AppButton';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { withSizeLessThan } from '../../hooks/useWindowSize';
+import Box from '@mui/material/Box'
+import Slider from '@mui/material/Slider'
+import AppButton from '../../components/AppButton'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { withSizeLessThan } from '../../hooks/useWindowSize'
 import SectionHead from '../../components/common/Head'
 import { getData } from '../../hooks/getData'
 import { numberWithCommas } from '../../hooks/tools'
 import SlideIn from '../../components/animation/SlideIn'
-import { pushDataLayer } from '../../helper/pushDataLayer';
+import { pushDataLayer } from '../../helper/pushDataLayer'
 import { useSelector, useDispatch } from 'react-redux'
-import { updateForm } from '../../hooks/form';
-
+import { updateForm } from '../../hooks/form'
 
 function valuetext(value) {
     return <p className="bg-red">{numberWithCommas(value)}</p>
@@ -21,17 +20,16 @@ function valuetext(value) {
 const theme = createTheme({
     palette: {
         primary: {
-            main: "#9FB0E4",
+            main: '#9FB0E4',
         },
     },
-});
-
+})
 
 const Revenue = ({ revenueSection }) => {
     const type = 'Expected Revenue'
     const dispatch = useDispatch()
-    const { form } = useSelector(state => state.contactForm)
-    const index = form.findIndex(i => i.type === type)
+    const { form } = useSelector((state) => state.contactForm)
+    const index = form.findIndex((i) => i.type === type)
     const { minimum, maximum } = revenueSection
     const sm = withSizeLessThan(720)
 
@@ -47,11 +45,30 @@ const Revenue = ({ revenueSection }) => {
         <div>
             <SectionHead title="Contact Us | Expected Revenue" />
             <ContactTitle title={revenueSection.question} />
-            {form[index].revenue && <SlideIn y={4}><h2 className='text-purple font-bold text-center text-4xl'>${numberWithCommas(form[index].revenue)}</h2></SlideIn>}
-            <div className="relative h-40 mx-auto mt-5" style={{ width: sm ? 300 : 600 }}>
+            {form[index].revenue && (
+                <SlideIn y={4}>
+                    <h2 className="text-purple font-bold text-center text-4xl">
+                        ${numberWithCommas(form[index].revenue)}
+                    </h2>
+                </SlideIn>
+            )}
+            <div
+                className="relative h-40 mx-auto mt-5"
+                style={{ width: sm ? 300 : 600 }}
+            >
                 <div className="absolute -left-5 top-0 flex flex-col z-0 justify-center items-center">
-                    <p className={`${!form[index].revenue ? 'text-red' : 'text-gray-500'} font-bold py-3`}>{numberWithCommas(minimum)}</p>
-                    <img src="/assets/img/contact/icons/blue_icons/expected_revenue.svg" alt="revenue" width={40} />
+                    <p
+                        className={`${
+                            !form[index].revenue ? 'text-red' : 'text-gray-500'
+                        } font-bold py-3`}
+                    >
+                        {numberWithCommas(minimum)}
+                    </p>
+                    <img
+                        src="/assets/img/contact/icons/blue_icons/expected_revenue.svg"
+                        alt="revenue"
+                        width={40}
+                    />
                 </div>
                 <div className="absolute bottom-0 left-1/2 transform -translate-x-2/4 flex items-center justify-center mx-auto">
                     <ThemeProvider theme={theme}>
@@ -66,18 +83,35 @@ const Revenue = ({ revenueSection }) => {
                                 marks
                                 color="primary"
                                 onChange={(e) => onGettingPrice(e)}
-
                             />
                         </Box>
                     </ThemeProvider>
                 </div>
                 <div className="absolute -right-5 top-0 z-0 flex flex-col justify-center items-center">
-                    <p className={`${!form[index].revenue ? 'text-red' : 'text-gray-500'} font-bold py-3`}>{numberWithCommas(maximum)}</p>
-                    <img src="/assets/img/contact/icons/blue_icons/expected_revenue.svg" alt="revenue" width={40} />
+                    <p
+                        className={`${
+                            !form[index].revenue ? 'text-red' : 'text-gray-500'
+                        } font-bold py-3`}
+                    >
+                        {numberWithCommas(maximum)}
+                    </p>
+                    <img
+                        src="/assets/img/contact/icons/blue_icons/expected_revenue.svg"
+                        alt="revenue"
+                        width={40}
+                    />
                 </div>
             </div>
             <div className="mx-auto w-56 py-10">
-                <AppButton title="Continue" width={200} bgColor="bg-blue-dark" bgColorHover="hover:bg-red" txtColor="text-white" link='/contact/services' clicked={() => ontoNextPage()} />
+                <AppButton
+                    title="Continue"
+                    width={200}
+                    bgColor="bg-blue-dark"
+                    bgColorHover="hover:bg-red"
+                    txtColor="text-white"
+                    link="/contact/services"
+                    clicked={() => ontoNextPage()}
+                />
             </div>
         </div>
     )
@@ -91,7 +125,9 @@ export const getStaticProps = async (ctx) => {
     } else {
         index = 0
     }
-    const props = await getData(`*[_type == 'interactiveForm'][${index}]{revenueSection}`)
+    const props = await getData(
+        `*[_type == 'interactiveForm'][${index}]{revenueSection}`
+    )
     return { props }
 }
 
