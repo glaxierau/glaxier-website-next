a8c6cfe42366d230d0b5b37a7180f65918b41fbb
nick-andriam <nick@glaxier.com.au>
2022-01-09 14:15:27 +0700

    service update + latest

---
+++ components/service/Steps.js
@@ -0,0 +1,15 @@
+import Img from 'next/image'
+
+export const Step = ({ title, desc, icon }) => {
+    return (
+        <section className="flex items-start justify-center m-2 w-full ">
+            <div className="bg-purple rounded-full overflow-hidden flex justify-center items-center p-3">
+                <Img {...icon} width={25} height={25} placeholder="empty" className='invert' />
+            </div>
+            <div className=" w-full ml-4 flex flex-col items-left justify-start ">
+                <p className="text-purple">{title}</p>
+                <p className="font-thin text-sm text-black-light pt-1 pb-2">{desc}</p>
+            </div>
+        </section>
+    )
+}
