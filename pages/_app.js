c85459846a278ef873f497e38961a71c9cff85cc
nick-andriam <nick@glaxier.com.au>
2022-01-13 11:45:57 +0700

    SVG hook

--- pages/_app.js
+++ pages/_app.js
@@ -9,14 +9,10 @@ import '../styles/hamburgers.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FetcherUI } from '../helper/fetcher';
-import { useSVGParser } from '../utils/parser';
-import SVG from '../components/common/SVG';
-import { useState } from 'react';
 
 
 function MyApp(props,) {
   const { Component, pageProps, router } = props
-  const url = 'https://cdn.sanity.io/images/a49e7mel/production/3e6b3336dbb7e54b0e16f3d393bc6f13d393845f-70x67.svg?w=2000&h=2000&fit=max'
 
   return (
     <>
