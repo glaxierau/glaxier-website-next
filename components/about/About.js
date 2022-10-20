3aeacf1c7fbfb6d19010298f9ef4c8582110feb0
Tan <thawin999@gmail.com>
2022-10-20 23:35:54 +1000

    adjusted banner image and about image on home page

--- components/about/About.js
+++ components/about/About.js
@@ -14,18 +14,48 @@ const About = (props) => {
         <>
             <div className="xl:h-screen lg:h-cscreen h-auto lg:flex lg:flex-row flex-col lg:px-14 px-5 lg:pt-0 pt-10 my-20">
                 <SlideIn className="lg:w-1/2 md:w-full w-full h-auto flex items-center justify-center">
-                    <Img src="/assets/img/home/aboutimg.png" priority width={550} height={550} alt="image" />
+                    <Img
+                        src="/assets/img/home/tan-with-computer-cropped.jpg"
+                        priority
+                        width={550}
+                        height={550}
+                        alt="image"
+                    />
                 </SlideIn>
-                <SlideIn delay={0.2} className="lg:w-1/2 w-full lg:mx-20 md:mx-10 mx-0 lg:mt-0 mt-20 flex flex-col items-center justify-center lg:py-20 py-5">
-                    <Title title={props.sectionTitle} lineColor="#CFD7F1" lineWidth="210" /> <br /> <br />
-                    <div className={` ${style.list_container} relative flex justify-between`}>
+                <SlideIn
+                    delay={0.2}
+                    className="lg:w-1/2 w-full lg:mx-20 md:mx-10 mx-0 lg:mt-0 mt-20 flex flex-col items-center justify-center lg:py-20 py-5"
+                >
+                    <Title
+                        title={props.sectionTitle}
+                        lineColor="#CFD7F1"
+                        lineWidth="210"
+                    />{' '}
+                    <br /> <br />
+                    <div
+                        className={` ${style.list_container} relative flex justify-between`}
+                    >
                         <div className={`${style.line} bg-red-dark`} />
-                        {props.checks.map((label) => <CheckIcon key={label} label={label} />)}
-                    </div> <br /> <br />
+                        {props.checks.map((label) => (
+                            <CheckIcon key={label} label={label} />
+                        ))}
+                    </div>{' '}
+                    <br /> <br />
                     <div className="mt-20 w-full xl:px-0 lg:px-5 md:px-36 px-0">
                         <BlockContent blocks={props.aboutDescription} />
-                    </div> <br />
-                    {withButton && <AppButton title={props.ctaButton.buttonText} width={200} height={40} bgColor="bg-blue" bgColorHover="hover:bg-red" txtColor="text-white" link={props.ctaButton.link} />}
+                    </div>{' '}
+                    <br />
+                    {withButton && (
+                        <AppButton
+                            title={props.ctaButton.buttonText}
+                            width={200}
+                            height={40}
+                            bgColor="bg-blue"
+                            bgColorHover="hover:bg-red"
+                            txtColor="text-white"
+                            link={props.ctaButton.link}
+                        />
+                    )}
                 </SlideIn>
             </div>
         </>
--- next.config.js
+++ next.config.js
@@ -1,17 +1,42 @@
 module.exports = {
     images: {
-        domain: ['cdn.sanity.io']
+        domain: ['cdn.sanity.io'],
     },
     i18n: {
         locales: ['en-au', 'th-th'],
-        defaultLocale: 'en-au'
+        defaultLocale: 'en-au',
     },
     async rewrites() {
         return [
             {
                 source: '/api/:paths*',
-                destination: 'https://a49e7mel.api.sanity.io/v1/data/query/production?query=',
-            }
+                destination:
+                    'https://a49e7mel.api.sanity.io/v1/data/query/production?query=',
+            },
+            {
+                source: '/blog',
+                destination: 'https://lp.glaxier.com.au/',
+            },
+            {
+                source: '/th/blog',
+                destination: 'https://lp.glaxier.com.au/th',
+            },
+            {
+                source: '/blog/:slug',
+                destination: 'https://lp.glaxier.com.au/:slug/',
+            },
+            {
+                source: '/th/blog/:slug',
+                destination: 'https://lp.glaxier.com.au/th/:slug/',
+            },
+            {
+                source: '/privacy-policy',
+                destination: 'https://lp.glaxier.com.au/privacy-policy/',
+            },
+            {
+                source: '/th/privacy-policy',
+                destination: 'https://lp.glaxier.com.au/th/privacy-policy/',
+            },
         ]
-    }
+    },
 }
