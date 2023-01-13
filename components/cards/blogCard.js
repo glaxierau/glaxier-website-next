302ab38b7ab71cb65c0cf736779f082822ab8bc4
glaxier <digital@glaxier.com.au>
2023-01-13 18:07:52 +0300

    added dummy blogs

---
+++ components/cards/blogCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function BlogCard() {
+    return (
+        <div className='w-[250px] h-[200px] drop-shadow-md bg-white m-auto my-10  cursor-pointer'>
+            <div className="h-[60%] bg-blue-100">
+                {/* Image */}
+            </div>
+            <div className="">
+                {/* <h3 className="text-lg font-bold">Title</h3> */}
+                {/* <p className="text-sm">Description</p> */}
+            </div>
+        </div>
+    )
+}
