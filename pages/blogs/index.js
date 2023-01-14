bf0f61013fdfa19669d24b00f02850fef128c655
glaxier <digital@glaxier.com.au>
2023-01-14 11:40:25 +0300

    added dummy blogs and article design

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import Slider from 'react-slick'
 import BlogCard from '../../components/cards/blogCard'
+import SectionHead from '../../components/common/Head'
+import styles from '../../styles/Blogs.module.css'
+
+
 
 
 export default function Blogs() {
-    const arr = Array.from(Array(20).keys())
+    const arr = Array.from(Array(18).keys())
+
     const [sliderStyle, setSliderStyle] = useState({
         dots: false,
         speed: 300,
@@ -45,14 +50,29 @@ export default function Blogs() {
     })
 
     return (
-        <div className='w-full h-full lg:flex justify-around'>
-            <Slider {...sliderStyle} arrows={true} customPaging={true} className="w-[90vw] m-auto">
+        <>
+            <SectionHead
+                title="Blogs"
+                description="Blogs"
+            />
+            <div className={`${styles.blogs_grid} lg:w-[82vw] md:w-[95vw] w-[92vw] h-auto mx-auto my-4`}>
                 {arr.map((index) => {
+                    return (
+                        <div key={index} className="bg-white shadow-sm">
+                            <aside className="h-[65%] bg-gray-300" />
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 bg-gray-200'>
+                {/* <Slider {...sliderStyle} arrows={true} customPaging={true} className="w-[90vw] m-auto"> */}
+                {/* {arr.map((index) => {
                     return (
                         <BlogCard key={index} />
                     )
-                })}
-            </Slider>
-        </div>
+                })} */}
+                {/* </Slider> */}
+            </div>
+        </>
     )
 }
