0c15941006c28d888de6b5c240de70a4cf4527fd
nick-andriam <nick@glaxier.com.au>
2023-01-25 12:50:27 +0300

    added language

--- components/common/LanguageSection.js
+++ components/common/LanguageSection.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
 import Flags from './Flags'
 
 const LanguageSection = () => {
-    const router = useRouter()
-    const langs = router.locales
-    const clang = router.locale
-
-    const getPosition = () => {
-        const value = document.getElementById("globeSVG").offsetLeft
-        setPosition(value - 20)
-    }
-
     return (
         <div className="flex">
             <Flags inline={true} size={30} />
