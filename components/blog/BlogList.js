9c5410c0fd40151d2ff58fee4ff7f11492e97205
tandeman77 <thawin999@gmail.com>
2023-04-28 16:11:33 +1000

    readded GTM

--- components/blog/BlogList.js
+++ components/blog/BlogList.js
@@ -7,58 +7,119 @@ import { useRouter } from 'next/router'
 import { urlFor } from '../../hooks/tools'
 import { timeStamp } from '../../helper/functions'
 
-
-
-export default function BlogList({ blogs, blogsToShow, }) {
+export default function BlogList({ blogs, blogsToShow }) {
     const router = useRouter()
     const page = +router.query.page || 1
 
     return (
         <div>
-
             {/* ------------ Listing Blogs -------------- */}
-            <div className={`${blogs.length <= 2 ? styles.single_blog_grid : styles.blogs_grid}
-                         h-auto mx-auto mb-20`} id='blogsList'>
-                {blogsToShow.length !== 0 && blogsToShow[page - 1]?.map(({ title, shortDescription, featuredImage, _createdAt, category, slug }, index) => {
-                    return (
-                        // <a>
-                        <Link key={index} href={`/blog/${category.slug}/${slug}`} passHref>
-                            <div key={index} className="relative bg-white shadow-sm cursor-pointer hover:shadow-xl hover:scale-[1.005] transition-all rounded-lg overflow-hidden">
-                                <aside className="relative h-[55%] bg-gray-300">
-                                    <Image
-                                        src={urlFor(featuredImage.image).height(400).url()}
-                                        className="object-cover bg-no-repeat"
-                                        layout="fill"
-                                        alt={featuredImage.image.alt}
-                                        title={featuredImage.image.title}
-                                        priority
-                                    />
-                                    <div className="absolute bottom-[10px] right-[10px]">
-                                        <div className='bg-red-400 text-gray-500 px-4 rounded-full'>
-                                            <p className='text-white  ' style={{ fontSize: 10 }}>{timeStamp(_createdAt)}</p>
-                                        </div>
+            <div
+                className={`${
+                    blogs.length <= 2
+                        ? styles.single_blog_grid
+                        : styles.blogs_grid
+                }
+                         h-auto mx-auto mb-20`}
+                id="blogsList"
+            >
+                {blogsToShow.length !== 0 &&
+                    blogsToShow[page - 1]?.map(
+                        (
+                            {
+                                title,
+                                shortDescription,
+                                featuredImage,
+                                _createdAt,
+                                category,
+                                slug,
+                            },
+                            index
+                        ) => {
+                            return (
+                                // <a>
+                                <Link
+                                    key={index}
+                                    href={`/blog/${category.slug}/${slug}`}
+                                    passHref
+                                >
+                                    <div
+                                        key={index}
+                                        className="relative bg-white shadow-sm cursor-pointer hover:shadow-xl hover:scale-[1.005] transition-all rounded-lg overflow-hidden"
+                                    >
+                                        <aside className="relative h-[55%] bg-gray-300">
+                                            <Image
+                                                src={urlFor(featuredImage.image)
+                                                    .height(400)
+                                                    .url()}
+                                                className="object-cover bg-no-repeat"
+                                                layout="fill"
+                                                alt={featuredImage.image.alt}
+                                                title={
+                                                    featuredImage.image.title
+                                                }
+                                                priority
+                                            />
+                                            <div className="absolute bottom-[10px] right-[10px]">
+                                                <div className="bg-red-400 text-gray-500 px-4 rounded-full">
+                                                    <p
+                                                        className="text-white  "
+                                                        style={{ fontSize: 10 }}
+                                                    >
+                                                        {timeStamp(_createdAt)}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </aside>
+                                        <a
+                                            href={`/blog/${category.slug}/${slug}`}
+                                            className={`overflow-hidden h-[45%] z-30 flex flex-col justify-center items-left
+                                            ${
+                                                index === 0
+                                                    ? blogs.length <= 2
+                                                        ? 'p-4'
+                                                        : 'lg:p-10 p-4'
+                                                    : 'p-4'
+                                            }`}
+                                        >
+                                            <h2
+                                                className={`${
+                                                    index === 0
+                                                        ? blogs.length <= 2
+                                                            ? 'text-md'
+                                                            : 'lg:text-xl md:text-base text-md lg:mb-2'
+                                                        : 'text-md'
+                                                } font-bold text-purple`}
+                                            >
+                                                {title}
+                                            </h2>
+                                            <p
+                                                className={`${styles.desc} 
+                                            ${
+                                                index === 0
+                                                    ? blogs.length <= 2
+                                                        ? 'md:text-sm text-es'
+                                                        : 'lg:text-base md:md:text-sm text-es'
+                                                    : 'md:text-sm text-es'
+                                            } text-gray-600`}
+                                            >
+                                                {shortDescription}
+                                            </p>
+                                        </a>
                                     </div>
-                                </aside>
-                                <a href={`/blog/${category.slug}/${slug}`} className={`overflow-hidden h-[45%] z-30 flex flex-col justify-center items-left
-                                            ${index === 0 ? blogs.length <= 2 ? 'p-4' : 'lg:p-10 p-4' : 'p-4'}`}>
-                                    <h2 className={`${index === 0 ? blogs.length <= 2 ? 'text-md' : 'lg:text-xl md:text-base text-md lg:mb-2' : 'text-md'} font-bold text-purple`}>
-                                        {title}
-                                    </h2>
-                                    <p className={`${styles.desc} 
-                                            ${index === 0 ? blogs.length <= 2 ? 'md:text-sm text-es' : 'lg:text-base md:md:text-sm text-es' : 'md:text-sm text-es'} text-gray-600`}
-                                    >{shortDescription}</p>
-                                </a>
-                            </div>
-                        </Link>
-                        // </a>
-                    )
-                })}
+                                </Link>
+                                // </a>
+                            )
+                        }
+                    )}
             </div>
-            {blogsToShow.length === 0 && <p className='text-red text-center w-full'>No Match Found</p>}
+            {blogsToShow.length === 0 && (
+                <p className="text-red text-center w-full">No Match Found</p>
+            )}
             {/* --------- Pagination ---------- */}
-            {blogsToShow.length >= 2 && <Pagination items={Math.ceil(blogs.length / 8)} />}
+            {blogsToShow.length >= 2 && (
+                <Pagination items={Math.ceil(blogs.length / 8)} />
+            )}
         </div>
     )
 }
-
-
--- pages/_app.js
+++ pages/_app.js
@@ -2,45 +2,43 @@ import Layout from '../components/layouts'
 import { Provider } from 'react-redux'
 import { AnimatePresence } from 'framer-motion'
 import store from '../redux/store'
-import NextNProgress from "nextjs-progressbar";
+import NextNProgress from 'nextjs-progressbar'
 import 'tailwindcss/tailwind.css'
 import '../styles/main.css'
 import '../styles/hamburgers.css'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { FetcherUI } from '../helper/fetcher';
-import Cookie from '../components/CookieConsent';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { FetcherUI } from '../helper/fetcher'
+import Cookie from '../components/CookieConsent'
+import gtm from '../utils/gtm'
 
-
-function MyApp(props,) {
-  const { Component, pageProps, router } = props
-  const tagManagerArgs = {
-    // gtmId: 'GTM-TMQD58L'
-  }
-  // const cookieConsent = cookieConsentValue()
-  // if (typeof window !== 'undefined') {
-  //   if (!cookieConsent) {
-  //     null
-  //   } else {
-  //     TagManager.initialize(tagManagerArgs)
-  //   }
-  // }
-  return (
-    <>
-      <Provider store={store}>
-        <FetcherUI lang={router.locale} />
-        <NextNProgress color="#9FB0E4" height={4} options={{ showSpinner: false }} />
-        <AnimatePresence key={router.route} exitBeforeEnter={true} initial={true}>
-          <section className={router.locale}>
-            <Layout>
-              <Component {...pageProps} />
-              <Cookie />
-            </Layout>
-          </section>
-        </AnimatePresence>
-      </Provider>
-    </>
-  )
+function MyApp(props) {
+    const { Component, pageProps, router } = props
+    gtm()
+    return (
+        <>
+            <Provider store={store}>
+                <FetcherUI lang={router.locale} />
+                <NextNProgress
+                    color="#9FB0E4"
+                    height={4}
+                    options={{ showSpinner: false }}
+                />
+                <AnimatePresence
+                    key={router.route}
+                    exitBeforeEnter={true}
+                    initial={true}
+                >
+                    <section className={router.locale}>
+                        <Layout>
+                            <Component {...pageProps} />
+                            <Cookie />
+                        </Layout>
+                    </section>
+                </AnimatePresence>
+            </Provider>
+        </>
+    )
 }
 
 export default MyApp
