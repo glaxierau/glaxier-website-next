a27baaccf6dbc57ffd8bd030ea02a399dac3abbc
tandeman77 <thawin999@gmail.com>
2023-04-28 16:33:49 +1000

    added fail proof for slug related blog

--- pages/api/services/index.js
+++ pages/api/services/index.js
@@ -1,12 +1,11 @@
-
 import SanityClient from '@sanity/client'
 
 export const services = async (req, res) => {
     const client = SanityClient({
-        projectId: 'a49e7mel',
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
         dataset: 'services',
         apiVersion: 'v1',
-        useCdn: false
+        useCdn: false,
     })
     const query = `*[_type == 'services]`
     const results = await client.fetch(query)
