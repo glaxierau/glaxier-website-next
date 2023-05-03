2d8dd316f8782858d5224fd32cd82f90378f3307
chatipum <chatipum@gmail.com>
2023-05-03 21:11:38 +0700

    ci: :art: adjust team photo size on mobile

--- components/icons/TeamBadge.js
+++ components/icons/TeamBadge.js
@@ -6,7 +6,7 @@ const TeamBadge = ({ name = 'Name', position = 'position', image, index }) => {
     let defaultImage =
         'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairTheCaesarSidePart&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Heather&eyeType=Wink&eyebrowType=Default&mouthType=Default&skinColor=Light'
     return (
-        <li className="lg:w-44 lg:h-44 md:w-44 md:h-44 w-36 h-36  relative rounded-full flex items-center justify-center overflow-hidden shadow-sm lg:m-2 m-1">
+        <li className="lg:w-44 lg:h-44 md:w-44 md:h-44 w-64 h-64  relative rounded-full flex items-center justify-center overflow-hidden shadow-sm lg:m-2 m-1">
             {image && (
                 <Img
                     {...sanityImage(image.image)}
